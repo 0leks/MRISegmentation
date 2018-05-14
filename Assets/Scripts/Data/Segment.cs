822eaff97c090bc0e1e212937a9afb47e014cfb2
Timothy P Hedstrom <thedstro@UCSD.EDU>
2018-05-14 16:07:00 -0700

    started refactoring to new system architecture

---
+++ Assets/Scripts/Data/Segment.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Segment : MonoBehaviour {
+
+    public struct SegmentData
+    {
+        public bool[,,] segment;
+    }
+
+}
---
+++ Assets/Scripts/Managers/ScanManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class manages creation, storing and modification of Scan Data.
+/// </summary>
+public class ScanManager : MonoBehaviour {
+
+    public Scans.ScansData m_ScanData;
+
+	// Use this for initialization
+	void Start () {
+        m_ScanData = new Scans.ScansData();
+	}
+	
+    /// <summary>
+    ///     Load textures, intensities, and a scan volume
+    /// </summary>
+    /// <param name="filepath"></param>
+    /// <param name="numLayers"></param>
+    public void LoadScanData (string filepath, int numLayers)
+    {
+        LoadScanData(Scans.LoadTexturesFromFiles(filepath, numLayers));
+    }
+
+    public void LoadScanData (List<Texture2D> scanTextures)
+    {
+        m_ScanData.scanTextures = new List<Texture2D>(scanTextures);
+        m_ScanData.scanIntensities = Scans.GetIntensitiesFromTextures(m_ScanData.scanTextures);
+        m_ScanData.scanVolume = Scans.GetPaddedTexture3DFromTextures(m_ScanData.scanTextures);
+    }
+
+}
